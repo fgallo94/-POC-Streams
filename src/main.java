245de1f53f623dabbdde1c35c3235570a7cfbd57
fgallo94 <federicogallo1994@hotmail.com>
2019-03-26 12:08:00 -0300

    two classes modified by the new issues

--- src/main.java
+++ src/main.java
@@ -1,11 +1,17 @@
 import controller.PersonController;
+import dto.Person;
+import java.util.List;
+import java.util.Arrays;;
 
 public class main {
 
     public static void main(String[] args) {
-        PersonController personController = new PersonController();
-        System.out.println(personController.executeOver21().toString());
-        System.out.println(personController.executeUnder18().toString());
-        System.out.println(personController.executeOver21WithSSN().toString());
+        List<Person> personList = Arrays.asList(new Person("Federico", "Gallo", 24, 38056968),
+                                                new Person("Anibal", "Matellan", 20, 42231645),
+                                                new Person("Guillermo", "Fernandez", 40, 12635172),
+                                                new Person("Carlos", "Martinez", 15, 46283790),
+                                                new Person("Fernando", "Suarez", 59, 6052168));
+        PersonController personController = new PersonController(personList);
+        personController.executeProof();
     }
 }
